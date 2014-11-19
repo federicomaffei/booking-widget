7eae035ae056aec97b7ac971fe302ee9821b312b
federicomaffei <fmaffei@opentable.com>
2014-11-19 18:05:00 +0000

    added availability search functionality.

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 'use strict'
 
-var express = require('express')
-var app = express()
-var bodyParser = require('body-parser')
+var express = require('express');
+var https = require('https');
+var app = express();
+var bodyParser = require('body-parser');
+
+var headers = {
+    'Authorization': 'token ' + process.env.API_DEV_KEY
+};
+
+var options = {
+    hostname: 'sandbox-api.opentable.com',
+    path: '',
+    method: '',
+    headers: headers
+};
+
+var results = [];
+
 
 app.set('view engine', 'jade');
 
-app.use(express.static(__dirname + '/assets'))
+app.use(express.static(__dirname + '/assets'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
@@ -14,18 +29,27 @@ app.use(bodyParser.urlencoded({
 
 app.get('/', function(req, res){
   res.render('index');
-})
+});
 
-app.post('/book', function(req, res){
-	console.log(req.body);
+app.post('/', function(req, res){
+    options.path = '/v1/restaurants/1/availability?dateTime=' + req.body.date + req.body.timeselect + '&partySize=' + req.body.partysize;
+    options.method = 'GET';
+    var request = https.request(options, function(availability){
+        availability.setEncoding('utf8');
+        availability.on('data', function (chunk) {
+            results = JSON.parse(chunk).results;
+            console.log(results);
+        });
+    });
+    request.end();
+    request.on('error', function(e) {
+        console.error(e);
+    });
 	res.render('index');
-})
+});
 
 var server = app.listen(process.env.DEV_PORT || 3000, function() {
-
-    var host = server.address().address
-    var port = server.address().port
-
-    console.log('Server started at http://%s:%s', host, port)
-
-})
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Server started at http://%s:%s', host, port);
+});
