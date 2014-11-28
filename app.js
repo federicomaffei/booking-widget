be69214d89dd89fd857708bbf4135d1d2a6f6005
federicomaffei <fmaffei@opentable.com>
2014-11-28 18:20:09 +0000

    refactored to use request module

--- app.js
+++ app.js
@@ -48,10 +48,9 @@ app.post('/provision_reservation', function(req, res){
         uri: 'https://sandbox-api.opentable.co.uk/v1/restaurants/1/reservations',
         method: 'POST',
         headers: {
-            'Authorization': 'token ' + process.env.WIDGET_API_KEY,
-            'Content-Type': 'application/json'
+            'Authorization': 'token ' + process.env.WIDGET_API_KEY
         },
-        form: req.body,
+        body: req.body,
         json: true
     }, function(error, response, body){
         console.log(body);
@@ -66,37 +65,14 @@ app.post('/confirm_reservation', function(req, res){
         uri: 'https://sandbox-api.opentable.co.uk/v1/restaurants/1/reservations/provisional/' + req.body.reservationToken + '/confirm',
         method: 'POST',
         headers: {
-            'Authorization': 'token ' + process.env.WIDGET_API_KEY,
-            'Content-Type': 'application/json'
+            'Authorization': 'token ' + process.env.WIDGET_API_KEY
         },
-        form: req.body
+        body: req.body,
+        json: true
     }, function(error, response, body){
         console.log(body);
         res.render('confirm-reservation', {confirmationMessage: body.message});
     })
-    //options.path = '/v1/restaurants/1/reservations/provisional/' + req.body.reservationToken + '/confirm';
-    //options.method = 'POST';
-    //var query = JSON.stringify(req.body);
-    //options.headers['Content-Type'] = 'application/json';
-    //options.headers['Content-Length'] = query.length;
-    //var request = https.request(options, function(confirmation){
-    //    var data = '';
-    //    confirmation.setEncoding('utf8');
-    //    confirmation.on('data', function(chunk){
-    //        data += chunk;
-    //    });
-    //    confirmation.on('end', function(){
-    //        var confirmationMessage = JSON.parse(data).message;
-    //        res.render('confirm-reservation', {confirmationMessage: confirmationMessage});
-    //    });
-    //});
-    //
-    //request.write(query);
-    //request.end();
-    //request.on('error', function(e) {
-    //    console.error(e);
-    //});
-
 });
 
 var server = app.listen(process.env.DEV_PORT || 3000, function() {
