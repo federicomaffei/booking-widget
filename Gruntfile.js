6d04ed3b54a2debe0335894817d864d589a631d7
Federico Maffei <federico.maffei@gmail.com>
2014-12-10 17:23:41 +0000

    added mochaTest task to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,12 +2,24 @@
 module.exports = function(grunt){
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-mocha-test');
 
     grunt.initConfig({
         jshint: {
             files: ['Gruntfile.js', 'assets/js/*.js', 'src/**/*.js', 'test/**/*.js']
+        },
+        mochaTest: {
+            test: {
+                options: {
+                    reporter: 'spec',
+                    recursive: true,
+                    timeout: 10000
+                },
+                src: ['test/**/*.js']
+            }
         }
     });
 
-    grunt.registerTask('default', ['jshint', 'npm test']);
+    grunt.registerTask('default', ['jshint', 'mochaTest']);
+    grunt.registerTask('test', ['mochaTest']);
 };
