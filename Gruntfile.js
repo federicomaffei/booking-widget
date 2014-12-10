b2058013e54091d667a51a971d870b989c6d8ea9
Federico Maffei <federico.maffei@gmail.com>
2014-12-10 15:06:50 +0000

    added grunt configuration for jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,11 @@
 module.exports = function(grunt){
 
+    grunt.initConfig({
+        jshint: {
+            all: ['Gruntfile.js', 'assets/js/*.js', 'src/**/*.js', 'test/**/*.js']
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.registerTask('default', ['jshint']);
 };
