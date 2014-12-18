65edce9f2f478ca745f4d222542e8ab5b379af91
Federico Maffei <federico.maffei@gmail.com>
2014-12-18 18:06:29 +0000

    added browserify and bundled front end code

---
+++ src/utilities/frontEndActions.js
@@ -0,0 +1,34 @@
+var tc = require('./timeCreator');
+var moment = require('moment');
+
+function addSlotsToOptions(slots){
+    for(var index = 0; index < slots.length; index++){
+        var slot = $('<option/>', {
+            text : tc.convertTime(slots[index]),
+            'value' : slots[index]
+        });
+        $('#timepicker').append(slot);
+    }
+}
+
+$(document).ready(function() {
+    $('#select-availability').submit(function() {
+        var restaurantId = $( 'select:first' ).val();
+        $('#select-availability').attr('action', '/search_availability/' + restaurantId);
+    });
+
+    $('#datepicker').datepicker({ dateFormat: 'yy-mm-dd', minDate: new Date()});
+    $('#datepicker').datepicker('setDate', new Date());
+
+    $('.available-hour-button').click(function() {
+        $('#offer-popup-'+ $(this).data('index')).toggle('slow');
+    });
+
+    var now = tc.createSlots(moment().format('HH'));
+    addSlotsToOptions(now, 22);
+    $('#datepicker').change(function(){
+        $('#timepicker').empty();
+        var slots = tc.createSlots(tc.setStart($(this).val()), 22);
+        addSlotsToOptions(slots);
+    });
+});
